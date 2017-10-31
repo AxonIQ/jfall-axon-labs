c67c8294ccaeebb1103543a0f1e99a1e7a02b131
Allard Buijze <allard.buijze@axoniq.io>
2017-10-31 09:45:44 +0100

    Added utility for starting DB and Gossip Server

--- chat-scaling-out/src/main/java/io/axoniq/labs/chat/Servers.java
+++ chat-scaling-out/src/main/java/io/axoniq/labs/chat/Servers.java
@@ -1,7 +1,9 @@
 package io.axoniq.labs.chat;
 
 import org.h2.tools.Server;
-import org.jgroups.stack.GossipRouter;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class Servers {
 
@@ -10,9 +12,23 @@ public class Servers {
         server.start();
         System.out.println("Database running on port 9092");
 
-        GossipRouter router = new GossipRouter("127.0.0.1", 12001);
-        router.start();
-        System.out.println("Gossip Router started on port 12001");
+        Method startMethod = null;
+        Object gossipRouter = null;
+        Method stopMethod = null;
+        try {
+            Class<?> gossipRouterClass = Servers.class.getClassLoader().loadClass("org.jgroups.stack.GossipRouter");
+            Constructor<?> constructor = gossipRouterClass.getDeclaredConstructor(String.class, int.class);
+            gossipRouter = constructor.newInstance("127.0.0.1", 12001);
+            startMethod = gossipRouterClass.getMethod("start");
+            stopMethod = gossipRouterClass.getMethod("stop");
+        } catch (ClassNotFoundException e) {
+            // Gossip Router not on class path
+        }
+
+        if (startMethod != null) {
+            startMethod.invoke(gossipRouter);
+            System.out.println("Gossip Router started on port 12001");
+        }
 
         System.out.println("Press any key to shut down");
         System.in.read();
@@ -20,7 +36,9 @@ public class Servers {
         System.out.println("Stopping database.");
         server.stop();
 
-        System.out.println("Stopping Gossip router");
-        router.stop();
+        if (stopMethod != null) {
+            System.out.println("Stopping Gossip Router");
+            stopMethod.invoke(gossipRouter);
+        }
     }
 }
